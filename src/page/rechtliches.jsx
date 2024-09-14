fb7f0061464e6a8c7fc5ff005c52b330bc8699ad
Bichler Bastian <bichlerbastian@gmail.com>
2024-09-14 18:00:35 +0200

    added Vite

---
+++ src/page/rechtliches.jsx
@@ -0,0 +1,16 @@
+// page/rechtliches.jsx
+
+import React from 'react';
+
+function Rechtliches() {
+  return (
+    <div>
+      <h1>Haftungsausschluss</h1>
+      <p>Wir übernehmen keine Verantwortung für die Richtigkeit, Vollständigkeit und Aktualität der bereitgestellten Informationen auf dieser Webseite. Haftungsansprüche gegen uns, die sich auf Schäden materieller oder ideeller Art beziehen, 
+        die durch die Nutzung oder Nichtnutzung der dargebotenen Informationen bzw. durch die Nutzung fehlerhafter und unvollständiger Informationen verursacht wurden, sind grundsätzlich ausgeschlossen.
+        Unser Forum speichert keine Cookies und erhebt keine persönlichen Daten ohne ausdrückliche Zustimmung.</p>
+    </div>
+  );
+}
+
+export default Rechtliches;
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './',  // Stellt sicher, dass alle Pfade relativ zur Root-Domain aufgelöst werden
+  build: {
+    rollupOptions: {
+      output: {
+        // Legt sicherere Namen für die Assets fest
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      }
+    }
+  }
+})
