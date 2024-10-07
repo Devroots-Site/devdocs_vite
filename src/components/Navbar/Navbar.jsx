ade9078aab797ace346aad8f923c62cd6c44d953
Bastian Bichler <bichlerbastian@gmail.com>
2024-10-07 20:12:07 +0200

    - Logo to navigate to /

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Navbar.css';  // Importiere die CSS-Datei
+import { Typography, Box } from '@mui/material';
 
 const DevRootsNavbar = () => {
   const navigate = useNavigate(); // useNavigate Hook initialisieren
 
   return (
     <nav className="navbar">
-      <h1 className="title">DevRoots Forum</h1>
+      <Box onClick={() => navigate('/')} sx={{ cursor: 'pointer' }}>
+        <Typography variant="h5" fontWeight={700} className="title">
+          DevRoots Forum
+        </Typography>
+      </Box>
       <button className="nav-item" onClick={() => navigate('/rechtliches')}>
         Rechtliches
       </button>
